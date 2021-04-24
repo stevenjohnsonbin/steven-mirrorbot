78d5f612f4b7c1a638850445a4ac8f3a6d340b1d
stevenjohnsonbin <79159290+stevenjohnsonbin@users.noreply.github.com>
2021-04-24 12:37:45 +0530

    Update mirror.py

--- bot/modules/mirror.py
+++ bot/modules/mirror.py
@@ -159,9 +159,9 @@ class MirrorListener(listeners.MirrorListeners):
                     share_url += '/'
                 if SHORTENER is not None and SHORTENER_API is not None:
                     siurl = requests.get('https://{}/api?api={}&url={}&format=text'.format(SHORTENER, SHORTENER_API, share_url)).text
-                    buttons.buildbutton("🗂️Index Link🗂️", siurl)
+                    buttons.buildbutton("🚀Index Link🚀", siurl)
                 else:
-                    buttons.buildbutton("🗂️Index Link🗂️", share_url)
+                    buttons.buildbutton("🚀Index Link🚀", share_url)
             if BUTTON_THREE_NAME is not None and BUTTON_THREE_URL is not None:
                 buttons.buildbutton(f"{BUTTON_THREE_NAME}", f"{BUTTON_THREE_URL}")
             if BUTTON_FOUR_NAME is not None and BUTTON_FOUR_URL is not None:
